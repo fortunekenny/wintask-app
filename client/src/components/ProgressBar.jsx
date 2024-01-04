418daa75205949f409ec4dee1aa08e084ac07dac
fortunekenny <gbephraim@gmail.com>
2024-01-04 04:23:50 +0100

    svg progresbar

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -4,31 +4,56 @@ const ProgressBar = ({ remainderTime, remainingTime }) => {
   let percent = Math.floor(
     ((remainingTime - remainderTime) / remainingTime) * 100
   );
-  let bgColor =
-    percent <= 25
-      ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 100%)`
-      : percent > 25 && percent <= 50
-      ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 50%, #57D9B2 100%)`
-      : percent > 50 && percent < 75
-      ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 25%, #57d9b2 50%, #57d95b 100%,)`
-      : percent > 75 && percent <= 100
-      ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 25%, #57D9B2 50%,#57d95b 75%, #d95b57 100% )`
-      : `linear-gradient(90deg, skyblue)`;
+  // let bgColor =
+  //   percent <= 25
+  //     ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 50%)`
+  //     : percent > 25 && percent <= 50
+  //     ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 50%, #57D9B2 100%)`
+  //     : percent > 50 && percent < 75
+  //     ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 25%, #57d9b2 50%, #57d95b 100%,)`
+  //     : percent > 75 && percent <= 100
+  //     ? `linear-gradient(90deg, #57a9d9 0%, #8b57d9 25%, #57D9B2 50%,#57d95b 75%, #d95b57 100% )`
+  //     : `linear-gradient(90deg, skyblue)`;
 
   return (
     <Wrapper>
       <h4>{percent}%</h4>
       <div className="container">
-        <div className="progress-bar">
-          <div
-            className="progress-bar-fill"
-            style={{
-              transform: `translateX(${percent - 100}%)`,
-              // width: `${percent}%`,
-              backgroundImage: bgColor,
-            }}
-          ></div>
-        </div>
+        <svg height="30" width="500">
+          <defs>
+            <linearGradient
+              id="e"
+              x1="40"
+              y1="210"
+              x2="460"
+              y2="210"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="steelblue" offset="1%" />
+              <stop stopColor="gold" offset="25%" />
+              {/* <stop stopColor="steelblue" offset="55%" /> */}
+              <stop stopColor="red" offset="75%" />
+            </linearGradient>
+          </defs>
+          <line
+            x1="10"
+            y1="15"
+            x2="300"
+            y2="15"
+            stroke="grey"
+            // strokeLinecap="round"
+            // strokeWidth="17"
+          />
+          <line
+            x1="10"
+            y1="15"
+            x2={percent * 3}
+            y2="15"
+            stroke="url(#e)"
+            // strokeLinecap="round"
+            // strokeWidth="15"
+          />
+        </svg>
       </div>
     </Wrapper>
   );
@@ -36,27 +61,13 @@ const ProgressBar = ({ remainderTime, remainingTime }) => {
 
 const Wrapper = styled.div`
   background: skyblue;
-  .container {
-    width: 300px;
-    height: 30px;
+  svg {
+    /* background: purple; */
   }
-  .progress-bar {
-    width: 100%;
-    height: 100%;
-    border-radius: 15px;
-    background-color: #e6e6e6;
-    overflow: hidden;
-  }
-  .progress-bar-fill {
-    height: 100%;
-    border-radius: 15px;
-    width: 100%;
+  line {
+    stroke-linecap: round;
+    stroke-width: 17;
     transition: all 0.5s ease-in-out;
-  }
-
-  h4 {
-    /* text-align: center; */
-    /* padding-top: 9px; */
   }
 `;
 
